72323005338469fba252efb8127f0ff2408a4b0c
eee1eli <166856755+eee1eli@users.noreply.github.com>
2026-03-01 22:43:51 +0200

    Add files via upload

---
+++ Lab2/lab2_example/Program.cs
@@ -0,0 +1,16 @@
+﻿    using System;
+    using lab2;
+
+    class Program
+    {
+        static void Main()
+        {
+            string text = "Hello World";
+            Console.WriteLine(MyFunctions.ReverseString(text));
+
+            int[] numbers = { 2, 1, 5, 9, 10 };
+            lab2.MyFunctions.SortArrayAscending(numbers);
+            MyFunctions.PrintArray(numbers);
+        
+        }
+    }
