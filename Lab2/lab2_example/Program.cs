f09e204cf978c018d887a7fc123e3d813afaae56
eee1eli <166856755+eee1eli@users.noreply.github.com>
2026-03-01 22:49:02 +0200

    Remove SortArrayAscending call from Main method
    Removed the call to SortArrayAscending from Main method.

--- Lab2/lab2_example/Program.cs
+++ Lab2/lab2_example/Program.cs
@@ -9,8 +9,8 @@
             Console.WriteLine(MyFunctions.ReverseString(text));
 
             int[] numbers = { 2, 1, 5, 9, 10 };
-            lab2.MyFunctions.SortArrayAscending(numbers);
             MyFunctions.PrintArray(numbers);
         
         }
     }
+
